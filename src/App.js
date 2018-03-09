6f28a793b5c39c2f865f900e1b1a1c1c2c59fe8b
Adam Hall <adam.hall@hitched.email>
2018-03-09 22:14:52 +0000

    Now with added rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {example} from './JiraToDot/unitExample';
+import {parseMultipleBlockers} from './JiraToDot/parse';
+import {toDotMultiple} from './JiraToDot/toDot';
+import Viz from 'viz.js';
 
 class App extends Component {
   render() {
+    var graph = toDotMultiple(parseMultipleBlockers(example));
+    //var svg = {__html: Viz("digraph { a -> b; }")};
+    var svg = {__html: Viz(graph)};
     return (
       <div className="App">
         <header className="App-header">
@@ -12,7 +19,10 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
+        <div dangerouslySetInnerHTML={svg} />
+          <p>nice!</p>
       </div>
     );
   }
