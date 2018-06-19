33fc8384dd51a406df7db07e9aefea50e00f2245
Adam Hall <adam.hall@hitched.email>
2018-06-19 20:49:22 +0100

    Adds login (and logout) to the UI

--- public/js/callApi.js
+++ public/js/callApi.js
@@ -1,16 +1,26 @@
 
 import {queryBuilder} from './queryBuilder.js';
 
-export async function callApi () {
+export async function callApi (login) {
   console.log("pingging");
   
   const api = "https://im-jira-api.herokuapp.com";
   const endpoint = queryBuilder();
-  const response = await fetch(`${api}/${endpoint}`);
+  const response = await fetch(`${api}/${endpoint}`, {
+    headers: login
+  });
+  /*
+  {
+      'username': 'adam@hitched.co.uk',
+      'password': 'HcRyQVSbNOQE74HADSe0A8A6'
+    }
+    */
+
   const body = await response.json();
   
   console.log(body);
 
   if (response.status !== 200) throw Error(body.message);
+  
   return body;
 }
--- public/js/index.js
+++ public/js/index.js
@@ -2,23 +2,38 @@
 import {parseBlockers, parseMultipleBlockers} from './parse.js';
 import {toDot, toDotMultiple, removeDashes} from './toDot.js';
 import {callApi} from './callApi.js';
+import {pageGetLogin, pageSetLogin, saveLogin, loadLogin, removeSavedLogin} from './pageData.js';
+
+
 
 function setLoadingState() {
     document.getElementById('root').innerHTML = "<p>... loading ...</p>";
 }
 
+function savePageLogin() {
+    var login = pageGetLogin();
+    saveLogin(login);
+    return login;
+}
+
+
 function go() {
     setLoadingState();
-    callApi().then((myData) => {
+    var login = savePageLogin();
+
+    callApi(login).then((myData) => {
         const data = toDotMultiple(parseMultipleBlockers(myData));
         console.log(data);
         document.getElementById('root').innerHTML = Viz(data);
     });
 }
-
+const login = loadLogin();
+pageSetLogin(login);
 go();
 
 document.getElementById('go').onclick = go;
+document.getElementById('save-login').onclick = savePageLogin;
+document.getElementById('delete-login').onclick = removeSavedLogin;
 
 
 
