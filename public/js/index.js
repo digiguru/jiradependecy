b25edbb7de4670c1bd08dc4a7bc29687d4ec6556
digiguru <digiguru@gmail.com>
2026-08-10 20:54:44 +0100

    Render dependency graph asynchronously with Viz.js v3

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import {parseMultipleBlockers} from './parse.js';
 import {toDot} from './toDot.js';
 import {remapTickets} from './statusMapper.js';
 import {callApi} from './callApi.js';
+import {renderGraph} from './renderGraph.js';
 import LoginUI from './UI/LoginUI.js';
 import DataUI from './UI/DataUI.js';
 import Storage from './Storage.js';
@@ -25,24 +26,22 @@ function deletePageLogin() {
     storage.deleteLogin()
     loginUI.login = {};
 }
-function go() {
+async function go() {
     dataUI.LoadingState();
-    var login = savePageLogin();
+    const login = savePageLogin();
+    const myData = await callApi(login);
 
-    callApi(login).then((myData) => {
-
-        const columnMappings = [
-            {'input': ['Backlog', 'Ready For Shaping', 'Ready for Development'], 
-                'output': {'colour': '#0000ff'}},
-            {'input': ['Doing', 'Review', 'Testing'], 
-                'output': {'colour': '#FFFF00'}},
-            {'input': ['Build', 'Released'], 
-                'output': {'colour': '#00FF00'}}
-        ];
-        const tickets = myData.issues;
-        const output = remapTickets(columnMappings, tickets);
-        const data = toDot(parseMultipleBlockers(output));
-        dataUI.Update(Viz(data));
-    });
+    const columnMappings = [
+        {'input': ['Backlog', 'Ready For Shaping', 'Ready for Development'],
+            'output': {'colour': '#0000ff'}},
+        {'input': ['Doing', 'Review', 'Testing'],
+            'output': {'colour': '#FFFF00'}},
+        {'input': ['Build', 'Released'],
+            'output': {'colour': '#00FF00'}}
+    ];
+    const tickets = myData.issues;
+    const output = remapTickets(columnMappings, tickets);
+    const data = toDot(parseMultipleBlockers(output));
+    dataUI.Update(await renderGraph(data));
 }
 go();
