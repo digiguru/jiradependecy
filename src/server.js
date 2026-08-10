e9f47af9f1b642f556f0d15fafc115c7642bfdf4
digiguru <digiguru@gmail.com>
2026-08-10 20:54:10 +0100

    Serve maintained Viz.js module

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const app = express();
-const path = require("path");
+const path = require('path');
 
+const app = express();
 const port = process.env.PORT || 4000;
 
+app.use('/vendor/viz', express.static(path.join(__dirname, '../node_modules/@viz-js/viz/dist')));
 app.use(express.static('public'));
-
-//app.use('/scripts', express.static(path.join(__dirname, '/node_modules/viz.js')));
-//Can't get importing of modules in node_moduels to work :(
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
